ec23c898ea31faf1f538259d09b6539ca34344ed
Sanabel8 <sanabeljamal97@gmail.com>
2021-06-06 18:32:59 +0300

    finished class01

---
+++ hourses/src/components/footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Footer extends React.Component {
+    render() {
+        return (
+            <footer>
+               
+           <h3>Sanabel Abu-Ezbaead</h3> 
+   
+             </footer>
+        )
+    }
+}
+
+export default Footer ;
