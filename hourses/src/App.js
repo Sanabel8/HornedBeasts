2ab3a2eda2ec9f95003848c9a89ca54fee27f84c
Sanabel8 <sanabeljamal97@gmail.com>
2021-06-08 18:52:04 +0300

    finished lab 3

--- hourses/src/App.js
+++ hourses/src/App.js
@@ -2,18 +2,83 @@ import React from 'react';
 import Footer from './components/footer';
 import Header from './components/Header';
 import Main from './components/main';
+import DataHorned from './components/Data.json';
+import SelectedBeast from './components/SelectedBeast';
 
-class App extends React.Component
-{
-  render(){
-    return(
+
+
+
+
+class App extends React.Component{
+  constructor(props) {
+    super(props)
+    this.state = {
+
+      beastData: DataHorned,
+      showPic: false,
+      itemSelected: {}
+
+
+    }
+
+  }
+
+  setShowPic = () => {
+    this.setState({
+      showPic: true
+    })
+
+  }
+
+  CloseFun = () => {
+    this.setState({
+      showPic: false
+
+    })
+
+  }
+
+
+  popUpCard = (title) => {
+    let itemSelected = DataHorned.find(card => {
+      if (card.title === title) {
+        return card;
+      }
+    })
+
+    this.setState({
+      showPic: true,
+      itemSelected: itemSelected
+    })
+  }
+
+
+
+  render() {
+    return (
       <div>
         <Header />
-        <Main />
-       <Footer />
+
+        <Main
+          beastData={this.state.beastData}
+          CloseFun={this.CloseFun}
+          popUpCard={this.popUpCard}
+
+        />
+
+        <Footer />
+
+        <SelectedBeast
+          beastData={this.state.beastData}
+          showPic={this.state.showPic}
+          CloseFun={this.CloseFun}
+          itemSelected={this.state.itemSelected}
+        />
       </div>
     )
   }
 }
 
-export default App ;
+export default App;
+
+ 
---
+++ hourses/src/components/SelectedBeast.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
+class SelectedBeast extends React.Component {
+
+
+    render() {
+
+        return (
+
+            <div>
+                <Modal showPic={this.props.showPic} onHide={this.props.CloseFun}>
+                    <Modal.Header closeButton>
+
+                        <Modal.Title>{this.props.itemSelected.title}</Modal.Title>
+                    </Modal.Header>
+
+                    <Card.Img variant="top" src={this.props.itemSelected.image_url} alt='img' title={this.props.itemSelected.title} />
+
+                    <Modal.Body>{this.props.itemSelected.description}</Modal.Body>
+                    <Modal.Footer>
+
+                        <Button variant="secondary" onClick={this.props.CloseFun}>
+                            Close</Button>
+
+                        <Button variant="primary" onClick={this.props.CloseFun}>
+                            Save Changes</Button>
+
+                    </Modal.Footer>
+                </Modal>
+                    )
+            </div>
+
+        )
+    }
+}
+
+export default SelectedBeast;
+
+// class SelectedBeast extends React.Component {
+
+//     constructor(props) {
+//         super();
+//         this.state = {
+//             numberOfPick: 0
+//         }
+//     }
+
+//     choosenPic = () => {
+//         // this.props.favoritePictures();
+//         let cont = this.state.numberOfPick
+//         this.setState({
+//             numberOfPick: cont += 1
+//         })
+//     }
+
+
+//     render() {
+//         console.log(this.props);
+//         return (
+//             <div>
+//                 <h2>{this.props.title}</h2>
+//                 <img onClick={this.choosenPic} style={{ width: '200px' }} src={this.props.image_url} alt={this.props.title} />
+//                 <p>{this.props.description}</p>
+
+
+//                 {/* <button onClick={this.choosenPic}>num of choosen</button> */}
+//                 <p>***{this.state.numberOfPick}***</p>
+
+
+//             </div>
+//         )
+//     }
+// }
+// export default SelectedBeast;
